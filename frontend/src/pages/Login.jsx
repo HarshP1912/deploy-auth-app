4f9e65e535e2fa8bfbfa61782b3e2c0b93118770
HarshP1912 <118930582+HarshP1912@users.noreply.github.com>
2025-12-30 13:08:23 +0530

    Update Login.jsx

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -23,7 +23,7 @@ function Login() {
             return handleError("Enter Valid Credentials");
         }
         try {
-            const url = "https://deploy-auth-app-api-ten.vercel.app/login";
+            const url = "https://deploy-auth-app-api-ten.vercel.app/auth/login";
             const response = await fetch(url, {
                 method: "POST",
                 headers: {
